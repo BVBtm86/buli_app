1409548052e17988d7133c36040ddda7b20cd474
BVBtm86 <omescu.mario.lucian@gmail.com>
2023-05-20 17:12:26 +0300

    error on top 10 value counts

--- page_scripts/stats_scripts/player_stats.py
+++ page_scripts/stats_scripts/player_stats.py
@@ -168,14 +168,16 @@ def player_top_statistics(data, season_filter, avg_players, stat_top10, type_top
     # ##### Markdown
     max_no_players = top10_plot_data['Team'].value_counts().max()
     if max_no_players > 1:
-        value_counts = top10_plot_data['Team'].value_counts().reset_index()
-        value_counts = value_counts.rename(columns={"index": "Team", "Team": "# Players"})
-        teams_no_top10 = value_counts[value_counts['# Players'] == max_no_players]['Team'].values
-        # print(teams_no_top10)
-        no_teams_top10 = len(teams_no_top10)
+        # value_counts = top10_plot_data['Team'].value_counts().reset_index()
+        # value_counts = value_counts.rename(columns={"index": "Team", "Team": "# Players"})
+        # teams_no_top10 = value_counts[value_counts['# Players'] == max_no_players]['Team'].values
+        # # print(teams_no_top10)
+        # no_teams_top10 = len(teams_no_top10)
+        # teams_top10 = ""
+        # for team in teams_no_top10:
+        #     teams_top10 += team + ", "
         teams_top10 = ""
-        for team in teams_no_top10:
-            teams_top10 += team + ", "
+        no_teams_top10 = 0
     else:
         teams_top10 = ""
         no_teams_top10 = 0
